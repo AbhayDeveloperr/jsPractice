2931dffd5b66cb615932acaf986b74cfb9f31320
guruabhay25 <guptaabhay2727@gmail.com>
2026-02-10 23:41:39 +0530

    Assignment Operators

--- Operators&ConditionalStat/script.js
+++ Operators&ConditionalStat/script.js
@@ -1,6 +1,41 @@
 //Operators
 
-//Arithmetic Operators, Addition
-let a = 2;
-let b = 8;
-console.log("a + b =",a + b);
+//Arithmetic Operators, Addition,Sub,Mul,Div,Modulo,Exponent
+// let a = 10;
+// let b = 3;
+// console.log("a + b =",a + b);
+// console.log("a - b =",a - b);
+// console.log("a * b =",a * b);
+// console.log("a / b =",a / b);
+// console.log("a % b =",a % b);
+// console.log("a ** b =",a ** b);
+
+// Unary Operators
+// let a = 5;
+// let b = 2;
+// console.log("a =",a, "b =",b);
+// a = a+1;     // a++  isko aise v likh sakte hai
+// b= b-1;      // b-- 
+// console.log(a);
+// console.log(b);
+
+//Post Increament,Decreament (a++, a--)
+// let a=6;
+// let b=4;
+// console.log("a++=",a++);    //Agle line me increament krega
+// console.log(a);             //Yha increament hoga 
+
+//Pre Increament,Decreament (++a, --a)
+// let a=6;
+// let b=4;
+// console.log("++a=",++a);   //Turant increament krega
+
+
+// Assignment Operators
+// let a = 5;
+// let b = 2;
+// a += 4;             //a = a + 4
+// console.log("a = ", a);
+          
+
+
