869c85c098eec3d90eb382448fd5cc831dbd2242
guruabhay25 <guptaabhay2727@gmail.com>
2026-02-13 00:14:09 +0530

    Practice Q on loopArray

--- Arrays/script.js
+++ Arrays/script.js
@@ -16,7 +16,28 @@
 // }
 
 //for of loops
-let heroes = ["Hulk","Spiderman","Ironman","Thor","Krishh"];
-for(let item of heroes){
-    console.log(item);
+// let heroes = ["Hulk","Spiderman","Ironman","Thor","Krishh"];
+// for(let item of heroes){
+//     console.log(item);
+// }
+
+
+
+//Practice Que on Loop in Array
+//Q1
+// let marks = [85,97,44,37,76,60];
+// let sum = 0;
+// for(let val of marks){
+//     sum = sum + val;
+// }
+// let avg = sum/marks.length;
+// console.log(`avg marks of the class = ${avg}`);
+
+
+//Q2
+let price = [250,645,300,900,50];
+for(let i=0; i<price.length; i++){
+    let offer = price[i] / 10;
+    price[i] = price[i] - offer;
 }
+console.log(`final price after offer is ${price}`);
