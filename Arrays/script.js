a87e00c4409fbf4d2985c126a52f73086850d286
guruabhay25 <guptaabhay2727@gmail.com>
2026-02-12 23:20:36 +0530

    LoopingInArray

--- Arrays/script.js
+++ Arrays/script.js
@@ -1,6 +1,22 @@
 //Arrays
-let marks = [98,96,87,89,78];
-console.log(marks);
+// let marks = [98,96,87,89,78];
+// console.log(marks[0]);   //indexing
+// marks[4] = 66;
+// console.log(marks);
 
-let heroes = ["Hulk","Spiderman","Ironman"];
-console.log(heroes);
+
+
+
+//Looping in Arrays
+
+//for loop
+// let heroes = ["Hulk","Spiderman","Ironman","Thor","Krishh"];
+// for(i=0; i<heroes.length;i++){
+//     console.log(heroes[i]);
+// }
+
+//for of loops
+let heroes = ["Hulk","Spiderman","Ironman","Thor","Krishh"];
+for(let item of heroes){
+    console.log(item);
+}
