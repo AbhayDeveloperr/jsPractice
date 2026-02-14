72dbeb1d9ae007ab330a377a5affc0a60e8f70e1
guruabhay25 <guptaabhay2727@gmail.com>
2026-02-14 23:54:58 +0530

    ArrowFunction

---
+++ Functions/script.js
@@ -0,0 +1,32 @@
+//Function with Parameter
+// function myFunction(msg){
+//     console.log(msg);
+// }
+// myFunction("I love JS");
+
+
+//Function to sum of two numbers
+// function sum(x,y){
+//     console.log(x+y);
+// }
+// sum(3,7);
+
+//using Return
+// function sum(a,b){
+//     s = a+b;
+//     return s;
+// }
+// let val = sum(3,2);
+// console.log(val);
+
+
+//Arrow Function 
+const sum = (a,b) =>{
+    return a+b;
+}
+console.log(sum(3,5));
+
+
+
+
+
