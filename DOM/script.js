922806aa3b42107bcb60621e742949ba736204e7
guruabhay25 <guptaabhay2727@gmail.com>
2026-02-17 22:26:20 +0530

    DOM basics

--- DOM/script.js
+++ DOM/script.js
@@ -1,2 +1,8 @@
-console.dir(document.body)  //Print Properties and Methods
-console.log(document.body);  //Print the tags Of Body tag
+// console.dir(document.body)    //Print Properties and Methods of body tag
+// console.log(document.body);   //Print the tags Of Body tag
+
+//We can change the color in JS
+document.body.style.background = "purple";
+
+//We can Change the Elements of a tag in JS
+document.body.childNodes[1].innerText = "abcd";
