ad566f9f935cb9043cc9e62cc33b41bf8b55a16f
guruabhay25 <guptaabhay2727@gmail.com>
2026-02-18 15:36:49 +0530

    Aise hi

--- DOM/script.js
+++ DOM/script.js
@@ -2,7 +2,28 @@
 // console.log(document.body);   //Print the tags Of Body tag
 
 //We can change the color in JS
-document.body.style.background = "purple";
+//document.body.style.background = "purple";
 
 //We can Change the Elements of a tag in JS
-document.body.childNodes[1].innerText = "abcd";
+//document.body.childNodes[1].innerText = "abcd";
+
+
+//DOM Manipulation
+
+// Selecting with id
+// let heading= document.getElementById("header");
+// console.dir(heading);
+
+// Selecting with class
+// let heading= document.getElementsByClassName("header-class");
+// console.dir(heading);
+
+// Selecting with class
+// let paras= document.getElementsByTagName("p");
+// console.dir(paras);
+
+function myFunction(x,y){
+    console.log(a*b);
+}
+myFunction(3,3);
+
