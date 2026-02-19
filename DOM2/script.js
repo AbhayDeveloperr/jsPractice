b43284195322d8bc233ada30a8156b94608134f5
guruabhay25 <guptaabhay2727@gmail.com>
2026-02-20 01:08:48 +0530

    AttributesDOM

--- DOM2/script.js
+++ DOM2/script.js
@@ -8,3 +8,17 @@ let para = document.querySelector(".line");    //for class
 console.log(para);
 let b = para.getAttribute("class");
 console.log(b);
+
+
+//setAttribute
+let para1 = document.querySelector(".line");    //for class
+console.log(para1);
+let c = para1.setAttribute("class","guruClass");
+console.log(c);
+
+//div.style
+let edit = document.querySelector("#box");
+console.log(edit);
+edit.style.backgroundColor = "green";
+edit.style.fontSize = "24px";
+edit.innerText = "abc";
