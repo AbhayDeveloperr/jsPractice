207d9afadcfbe15bc9950688f80c00d865f89f65
guruabhay25 <guptaabhay2727@gmail.com>
2026-02-21 12:11:52 +0530

    EventHndling

---
+++ Events/script.js
@@ -0,0 +1,14 @@
+//Event handling using JS
+let btn1 = document.querySelector("#btn1");       //accesing btn1
+
+btn1.onclick = () =>{
+    console.log("btn1 was clicked")
+    let a = 25;
+    a++;
+    console.log(a);
+}
+
+let div = document.querySelector("div");
+div.onmouseover = () =>{
+    console.log("You are inside div");
+}
