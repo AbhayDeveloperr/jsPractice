28a8bd9baea0011b9b766cf295b4c0aa06ac4c5d
guruabhay25 <guptaabhay2727@gmail.com>
2026-02-28 15:24:35 +0530

    Callback & Callback Hell

--- Callback,Promises & Async await/script.js
+++ Callback,Promises & Async await/script.js
@@ -10,10 +10,35 @@ console.log("1");
 console.log("2");
 setTimeout(()=>{
     console.log("Hello");
-},5000);
+},3000);
 
 console.log("3");
 console.log("4");
 
 
 
+//callback -->
+const hello = ()=>{
+    console.log("hello guys");
+}
+
+setTimeout(hello,4000);          //here, Hello is a callback fn 
+
+
+//Callback Hell -->       nested callback
+function getId(dataId,getNextData){
+    setTimeout(()=>{
+        console.log("data =",dataId);
+        if(getNextData){
+            getNextData();
+        };
+    },5000)
+};
+
+getId(1,()=>{
+    getId(2,()=>{
+        getId(3,()=>{
+            getId(4);
+        })
+    });
+});
